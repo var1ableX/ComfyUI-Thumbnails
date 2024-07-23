119de5e756695c34811ac054b4819c66dd3587e1
var1ableX <raj.kotecha@gmail.com>
2024-07-23 14:57:37 -0400

    removed references to node_helpers

--- ComfyUIThumbnails.py
+++ ComfyUIThumbnails.py
@@ -5,10 +5,14 @@ from aiohttp import web
 import aiohttp
 # import json
 # import zipfile
+import torch
 import urllib.request
 import pathlib
 from urllib.parse import unquote
-
+from PIL import ImageOps, Image, ImageDraw, ImageSequence
+import numpy as np
+import comfy
+from io import BytesIO
 from server import PromptServer
 import manager_core as core
 import cm_global
@@ -106,7 +110,8 @@ class LoadImageThumbnails:
   def load_image(self, image):
     image_path = folder_paths.get_annotated_filepath(image)
     
-    img = node_helpers.pillow(Image.open, image_path)
+    # Open the image without using node_helpers
+    img = Image.open(image_path)
     
     output_images = []
     output_masks = []
@@ -115,37 +120,39 @@ class LoadImageThumbnails:
     excluded_formats = ['MPO']
     
     for i in ImageSequence.Iterator(img):
-      i = node_helpers.pillow(ImageOps.exif_transpose, i)
+        # Transpose the image to handle EXIF orientation without using node_helpers
+        i = ImageOps.exif_transpose(i)
 
-      if i.mode == 'I':
-        i = i.point(lambda i: i * (1 / 255))
-      image = i.convert("RGB")
+        if i.mode == 'I':
+            i = i.point(lambda i: i * (1 / 255))
+        image = i.convert("RGB")
 
-      if len(output_images) == 0:
-        w = image.size[0]
-        h = image.size[1]
-      
-      if image.size[0] != w or image.size[1] != h:
-        continue
-      
-      image = np.array(image).astype(np.float32) / 255.0
-      image = torch.from_numpy(image)[None,]
-      if 'A' in i.getbands():
-        mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-        mask = 1. - torch.from_numpy(mask)
-      else:
-        mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-      output_images.append(image)
-      output_masks.append(mask.unsqueeze(0))
+        if len(output_images) == 0:
+            w = image.size[0]
+            h = image.size[1]
+
+        if image.size[0] != w or image.size[1] != h:
+            continue
+
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+        if 'A' in i.getbands():
+            mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+            mask = 1. - torch.from_numpy(mask)
+        else:
+            mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
+        output_images.append(image)
+        output_masks.append(mask.unsqueeze(0))
 
     if len(output_images) > 1 and img.format not in excluded_formats:
-      output_image = torch.cat(output_images, dim=0)
-      output_mask = torch.cat(output_masks, dim=0)
+        output_image = torch.cat(output_images, dim=0)
+        output_mask = torch.cat(output_masks, dim=0)
     else:
-      output_image = output_images[0]
-      output_mask = output_masks[0]
+        output_image = output_images[0]
+        output_mask = output_masks[0]
 
     return (output_image, output_mask)
+
 
   @classmethod
   def IS_CHANGED(s, image):
